a4e8e748d1dc78cb4d9b2eb4b6f70064c5263b9f
oslusarczyk <oskar.slusarczyk77@gmail.com>
2019-02-06 21:23:36 +0100

    new animation

---
+++ scripts/animation.js
@@ -0,0 +1,15 @@
+const text = 'front-end web developer'
+const paragraph = document.querySelector(".background p")
+
+const textIndex = [...text]
+
+let index = 0;
+
+const animation = () => {
+    paragraph.textContent += text[index];
+    index++;
+
+    if (index == 23) clearInterval(indexInterval);
+
+}
+const indexInterval = setInterval(animation, 250)
