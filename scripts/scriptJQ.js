e423d791efc306a8f8e9f91d46c4e2911e58fb24
oslusarczyk <oskar.slusarczyk77@gmail.com>
2019-02-06 23:26:43 +0100

    scroll

--- scripts/scriptJQ.js
+++ scripts/scriptJQ.js
@@ -1,5 +1,3 @@
-
-
 $(window).scroll(function () {
     const scroll = $(window).scrollTop();
     const windowHeight = $(window).height()
@@ -12,9 +10,31 @@ $(window).scroll(function () {
     const $skills = $('.skills');
     const skillFromTop = $skills.offset().top
     const skillHeight = $skills.outerHeight()
-
-    if( scroll > skillFromTop + skillHeight - windowHeight){
+    if (scroll > skillFromTop + skillHeight - windowHeight) {
         $skills.addClass("active");
     }
-});
 
+    const $portfolios = $('.portfolios');
+    const portfoliosFromTop = $portfolios.offset().top
+    const portfoliosHeight = $portfolios.outerHeight()
+
+    if (scroll > portfoliosFromTop + portfoliosHeight - windowHeight) {
+        $portfolios.addClass("active");
+    }
+
+    const $aboutme = $('.aboutme');
+    const aboutmeFromTop = $aboutme.offset().top
+    const aboutmeHeight = $aboutme.outerHeight()
+
+    if (scroll > aboutmeFromTop + aboutmeHeight - windowHeight) {
+        $aboutme.addClass("active");
+    }
+
+    const $contact = $('.contact');
+    const contactFromTop = $contact.offset().top
+    const contactHeight = $contact.outerHeight()
+
+    if (scroll > contactFromTop + contactHeight - windowHeight) {
+        $contact.addClass("active");
+    }
+});
